2aa46010f0730d0e96f94c109f3848f0d6e52269
Natanael Filho <filhonatanael01@gmail.com>
2025-06-17 00:10:30 -0300

    fix gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/party-combo-creator' : '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: '/party-combo-creator/',
   plugins: [
     react(),
     mode === 'development' &&
